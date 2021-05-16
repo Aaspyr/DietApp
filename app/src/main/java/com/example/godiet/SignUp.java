7a85ad5d9bf81918d4fb18a79488200acf0bb29a
Aleksandra Pawelska <aaspyr@gmail.com>
2021-05-16 21:44:15 +0200

    Fix java class

---
+++ app/src/main/java/com/example/godiet/SignUp.java
@@ -0,0 +1,11 @@
+package com.example.godiet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);}
+}
