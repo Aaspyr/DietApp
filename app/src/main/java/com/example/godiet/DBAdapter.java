1ad63d199e1303467e5a9346b8c00740c729ed8e
Aleksandra Pawelska <aaspyr@gmail.com>
2021-05-16 12:21:18 +0200

    Add to DB: Food table , Categories table

--- app/src/main/java/com/example/godiet/DBAdapter.java
+++ app/src/main/java/com/example/godiet/DBAdapter.java
@@ -1,6 +1,7 @@
 package com.example.godiet;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,14 +12,14 @@ import android.content.ContentValues;
 public class DBAdapter {
     /*------------*/
     private static final String databaseName = "dietapp";
-    private static final int databaseVersion = 1;
+    private static final int databaseVersion = 11;
 
     /*------------*/
     private final Context context;
     private DatabaseHelper DBHelper;
     private SQLiteDatabase db;
 
-    /*------------*/
+    /*Class DbAdapter-------*/
     public DBAdapter(Context ctx){
         this.context = ctx;
         DBHelper = new DatabaseHelper(context);
@@ -35,6 +36,38 @@ public class DBAdapter {
         public void onCreate(SQLiteDatabase db){
             try {
                 //Create tables
+                db.execSQL("CREATE TABLE IF NOT EXISTS categories (" +
+                        " category_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        " category_name VARCHAR," +
+                        " category_parent_id INT," +
+                        " category_icon VARCHAR," +
+                        " category_note VARCHAR);");
+
+                db.execSQL("CREATE TABLE IF NOT EXISTS food (" +
+                        " food_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " food_name VARCHAR," +
+                        " food_manufactor_name VARCHAR," +
+                        " food_serving_size DOUBLE," +
+                        " food_serving_mesurment VARCHAR," +
+                        " food_serving_name_number DOUBLE," +
+                        " food_serving_name_word VARCHAR," +
+                        " food_energy DOUBLE," +
+                        " food_proteins DOUBLE," +
+                        " food_carbohydrates DOUBLE," +
+                        " food_fat DOUBLE," +
+                        " food_energy_calculated DOUBLE," +
+                        " food_proteins_calculated DOUBLE," +
+                        " food_carbohydrates_calculated DOUBLE," +
+                        " food_fat_calculated DOUBLE," +
+                        " food_user_id INT," +
+                        " food_barcode VARCHAR," +
+                        " food_category_id INT," +
+                        " food_thumb VARCHAR," +
+                        " food_image_a VARCHAR," +
+                        " food_image_b VARCHAR," +
+                        " food_image_c VARCHAR," +
+                        " food_notes VARCHAR);");
+
             }
             catch (SQLException e){
                 e.printStackTrace();
@@ -44,11 +77,12 @@ public class DBAdapter {
         @Override
         public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
 
-            //db.execSQL("Drop table if exists" + databaseTableNotes);
+            db.execSQL("DROP TABLE IF EXISTS categories");
+            db.execSQL("DROP TABLE IF EXISTS food");
             onCreate(db);
 
             String TAG = "Tag";
-            Log.w(TAG, "Upgrade db form version" + oldVersion + "to" + newVersion + " which will destrou all old data");
+            Log.w(TAG, "Upgrade db form version" + oldVersion + "to" + newVersion + " which will destroy all old data");
         }
     }
 
@@ -63,5 +97,20 @@ public class DBAdapter {
 
     public void close() {
       DBHelper.close();
+    }
+
+    /* Insert data */
+    public void insert(String table, String fields, String values){
+    db.execSQL("INSERT INTO " + table + "(" + fields + ") VALUES ("+ values +")") ;
+    }
+
+    /*Count */
+    public int count(String table)
+    {
+        Cursor mCount = db.rawQuery("SELECT COUNT(*) FROM " + table + "", null);
+        mCount.moveToFirst();
+        int count= mCount.getInt(0);
+        mCount.close();
+        return count;
     }
 }
