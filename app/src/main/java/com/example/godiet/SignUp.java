fa84da57f6dede582139f7267de8998947c0209f
kamilkaz6 <kamilkaz6@wp.pl>
2021-05-19 20:36:45 +0200

    Set date and year of birthday

--- app/src/main/java/com/example/godiet/SignUp.java
+++ app/src/main/java/com/example/godiet/SignUp.java
@@ -1,11 +1,55 @@
 package com.example.godiet;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class SignUp extends AppCompatActivity {
+
+    /*Variables */
+    private String[] arraySpinnerDOBDay = new String[31];
+    private String[] arraySpinnerDOBYear = new String[100];
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);}
+        setContentView(R.layout.sign_up);
+
+        /* Fill numbers for date of birth days*/
+        int human_counter = 0;
+        for(int x=0;x<31;x++){
+            human_counter=x+1;
+            this.arraySpinnerDOBDay [x] = "" +human_counter;
+        }
+
+        Spinner spinnerDOBDay = (Spinner) findViewById(R.id.spinnerDOBDay);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinnerDOBDay);
+        spinnerDOBDay.setAdapter(adapter);
+
+        /* Fill numbers for date of birth year */
+        //get current year, month and data
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int end = year-100;
+        int index = 0;
+        for(int x=year;x>end;x--){
+            this.arraySpinnerDOBYear[index] = "" + x;
+            index++;
+        }
+
+        Spinner spinnerDOBYear = (Spinner) findViewById(R.id.spinnerDOBYear);
+        ArrayAdapter<String> adapterYear = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinnerDOBYear);
+        spinnerDOBYear.setAdapter(adapterYear);
+
+    }
+
 }
